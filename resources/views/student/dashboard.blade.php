b8305b82b4b2f240992eeadd8091bd40052f7972
Hasan Yagout <yagouthasan@gmail.com>
2024-11-17 21:11:52 +0300

    fix things

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -7,7 +7,7 @@
         <span class="d-block fs-2 text-black text-center"> {{$student->first_name.' '.$student->last_name}}</span>
     </x-wrapper>
     <div class="row">
-        <div class="col-lg-4">
+        <div class=" col-xl-4 col-lg-6">
             <x-wrapper title="">
                 <h1 class="text-black">news</h1>
                 <ul>
@@ -17,8 +17,8 @@
                                 <span class="fw-300 fw-600 text-black"> {{$new->title}}</span>
                                 <span> {!! $new->description !!}</span>
                             </section>
-                            <span class="fw-300 fw-600 text-black">
-{{ \Illuminate\Support\Carbon::parse($new->created_at)->format('Y-m-d') }}
+                            <span class="fw-300 fw-600 text-third-color">
+                            {{ \Illuminate\Support\Carbon::parse($new->created_at)->format('Y-m-d') }}
                         </span>
 
                         </li>
@@ -28,20 +28,20 @@
             </x-wrapper>
 
         </div>
-        <div class="col-lg-4">
+        <div class=" col-xl-4 col-lg-6">
             <x-wrapper title="">
                 <h1 class="text-black">notifications</h1>
-                <ul>
+                <ul style="height: 250px" class="overflow-auto">
                     @foreach($attentions as $attention)
-                        <section class="d-flex justify-content-between">
-                            <h6>{{$attention->title}}</h6>
-                            <h6>{{$attention->due_date}}</h6>
+                        <section class="border border-2 d-flex justify-content-between mb-10 p-13">
+                            <h6 class="text-black">{{$attention->title}}</h6>
+                            <h6 class="text-secondary-color text-third-color">{{\Illuminate\Support\Carbon::parse($attention->due_date)->toDateString()}}</h6>
                         </section>
                     @endforeach
                     @foreach($announcements as $announcement)
-                        <section class="d-flex justify-content-between">
-                            <h6>{{$announcement->title}}</h6>
-                            <h6>{{\Illuminate\Support\Carbon::parse($announcement->created_at)->toDateString()}}</h6>
+                        <section class="border border-2 d-flex justify-content-between mb-10 p-13">
+                            <h6 class=" text-black">{{$announcement->title}}</h6>
+                            <h6 class="text-secondary-color text-third-color">{{\Illuminate\Support\Carbon::parse($announcement->created_at)->toDateString()}}</h6>
                         </section>
                     @endforeach
 
